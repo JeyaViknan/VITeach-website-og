54da3641f267286c53ad919dd4834705ea011799
Kalyanasundaram V <112867527+Kalyanasundaram12@users.noreply.github.com>
2023-09-02 12:26:26 +0530

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+$(window).on('load', function() { 
+  $('#status').delay(400).fadeOut('slow'); 
+  $('#preloader').delay(400).fadeOut('slow');  
+  $('body').delay(400).css({'overflow':'visible'});
+}) 
