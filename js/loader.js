287b56aae504ae27fd012efcea9a5a01a1a43862
Kalyanasundaram V <112867527+Kalyanasundaram12@users.noreply.github.com>
2024-01-23 19:51:00 +0000

    Changes done

--- js/loader.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function() { 
-  $('#status').delay(400).fadeOut('slow'); 
-  $('#preloader').delay(400).fadeOut('slow');  
-  $('body').delay(400).css({'overflow':'visible'});
-}) 
